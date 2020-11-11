242ee7237a9e0e39ff1287bfe628c19dc9d6c444
Marc-Anthony Mitchell <mitchellmarcus297@gmail.com>
2020-11-10 23:22:56 -0500

    Added initial starter files

--- superheroes.php
+++ superheroes.php
@@ -1,6 +1,8 @@
 <?php
+ header("Access-Control-Allow-Origin: *");
 
 $superheroes = [
+    
   [
       "id" => 1,
       "name" => "Steve Rogers",
@@ -63,8 +65,27 @@ $superheroes = [
   ], 
 ];
 
-?>
-
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
+    if(!empty($_GET['query'])){
+      $sanitized_query = filter_var($_GET['query'], FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_FLAG_NO_ENCODE_QUOTES);
+      echo "<script>console.log(\" $sanitized_query \");</script>";
+      $not_found = 0;
+      foreach($superheroes as $item){
+        if(($item['name'] === $sanitized_query) || ($item['alias'] === $sanitized_query)){?>
+          <h3><?= $item['alias'];?></h3>
+          <h4>A.K.A. <?= $item['name'];?></h4>
+          <p><?= $item['biography'];?></p>
+        <?php
+        } else {
+          $not_found++;
+          if($not_found === 10){
+            echo "<h6 class=\"not-found\">SUPERHERO NOT FOUND</h6>";
+          }
+          echo "<script>console.log(\"testing\");</script>";
+        }
+      }
+    } else {?>
+    
 <ul>
 <?php foreach ($superheroes as $superhero): ?>
   <li><?= $superhero['alias']; ?></li>
